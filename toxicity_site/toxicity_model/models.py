5767505257ed58d3bd31627c86521019efc0c438
АД <degterevastasa@gmail.com>
2023-05-20 09:33:01 +0300

    created user

--- toxicity_site/toxicity_model/models.py
+++ toxicity_site/toxicity_model/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class SiteUser(models.Model):
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
+    requests_count = 0
