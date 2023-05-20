24976fab3b06486c568b2dfd2d09cb6ab8d03f19
АД <degterevastasa@gmail.com>
2023-05-20 12:16:18 +0300

    refactoring

--- toxicity_site/toxicity_model/forms.py
+++ toxicity_site/toxicity_model/forms.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
-    # email = forms.EmailField(required=True)
     username = forms.CharField(label='Name',
                                widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Password',
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Password Confirmation',
+    password2 = forms.CharField(label='Repeat Password',
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
