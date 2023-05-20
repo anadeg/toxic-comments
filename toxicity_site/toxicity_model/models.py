ecd24a9fb8925d4e0953d89a996b846a140efc69
АД <degterevastasa@gmail.com>
2023-05-20 11:11:30 +0300

    refactored

--- toxicity_site/toxicity_model/models.py
+++ toxicity_site/toxicity_model/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 # Create your models here.
 class SiteUser(models.Model):
-    name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
     password = models.CharField(max_length=50)
     requests_count = 0
