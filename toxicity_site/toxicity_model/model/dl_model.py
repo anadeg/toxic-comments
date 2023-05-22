4496cf3c50548f86fca1b8bdc2c204b55e52059d
АД <degterevastasa@gmail.com>
2023-05-22 13:12:10 +0300

    refactored

--- toxicity_site/toxicity_model/model/dl_model.py
+++ toxicity_site/toxicity_model/model/dl_model.py
@@ -16,47 +16,65 @@ class ToxicityModel:
     data_file = os.path.join(DIR_NAME, "comments.csv")
     max_features = 20000
     sequence_length = 250
-    embedding_dim = 9
-    cnn_epochs = 5
+    # sequence_length = 150
+    embedding_dim = 16
+    # cnn_epochs = 5
+    cnn_epochs = 2
 
     def __init__(self):
-        self.data = pd.read_csv(self.data_file)
+        # id,comment_text
+        # toxic, severe_toxic, obscene, threat, insult, identity_hate
+        self.data = pd.read_csv(self.data_file,
+                                dtype={"id": "category",
+                                       # "comment_text": "category",
+                                       "toxic": "int8",
+                                       "severe_toxic": "int8",
+                                       "obscene": "int8",
+                                       "threat": "int8",
+                                       "insult": "int8",
+                                       "identity_hate": "int8"},
+                                encoding='utf-8')
         self.X = self.data["comment_text"]
         self.Y = self.data[self.data.columns[2:]]
-        self.cnn_model = None
 
-    def vectorize(self, x):
-        vectorize_layer = tensorflow.keras.layers.TextVectorization(
+        self.cnn_model = Sequential()
+        self.cnn_model.add(Embedding(self.max_features + 1, self.embedding_dim))
+        self.cnn_model.add(Dropout(0.2))
+        self.cnn_model.add(GlobalAveragePooling1D())
+        self.cnn_model.add(Dropout(0.2))
+        self.cnn_model.add(Dense(16, activation='relu'))
+        self.cnn_model.add(Dense(6, activation='sigmoid'))
+
+        self.cnn_model.compile(loss='binary_crossentropy',
+                               optimizer='Adam',
+                               metrics=['accuracy'])
+
+        self.vectorize_layer = tensorflow.keras.layers.TextVectorization(
             standardize=self.custom_standardization,
             split='whitespace',
             max_tokens=self.max_features,
             output_mode='int',
             output_sequence_length=self.sequence_length,
             encoding='utf-8')
-        vectorize_layer.adapt(self.data['comment_text'])
+        self.vectorize_layer.adapt(self.X)
 
-        return np.array(vectorize_layer(x))
+    def vectorize(self, x):
+        return np.array(self.vectorize_layer(x))
 
     def fit(self):
         self.X = self.vectorize(self.X)
-
-        self.cnn_model = Sequential()
-        self.cnn_model.add(Embedding(self.max_features + 1, self.embedding_dim))
-        self.cnn_model.add(GlobalAveragePooling1D())
-        self.cnn_model.add(Dense(16, activation='relu'))
-        self.cnn_model.add(Dense(6, activation='sigmoid'))
-
-        self.cnn_model.compile(loss='binary_crossentropy',
-                          optimizer='Adam',
-                          metrics=['accuracy'])
-
-        history = self.cnn_model.fit(self.X,
-                                     self.Y,
-                                     epochs=self.cnn_epochs,
-                                     batch_size=512)
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.Y, test_size=0.2)
+        self.cnn_model.fit(X_train,
+                           y_train,
+                           epochs=self.cnn_epochs,
+                           batch_size=512,
+                           validation_data=(X_test, y_test))
 
     def predict(self, x):
+        x = np.array([x]).reshape(1, -1)
         x = self.vectorize(x)
+
+        # print(x)
 
         cnn_prediction = self.cnn_model.predict(x)
         return cnn_prediction
@@ -73,8 +91,9 @@ if __name__ == "__main__":
     tm = ToxicityModel()
     tm.fit()
     while True:
+        print("=============================")
         to_test = input()
     # to_test = "FUCK YOUR FILTHY MOTHER IN THE ASS, DRY!"
 
-        result = tm.predict(np.array([to_test]).reshape(1, -1))
+        result = tm.predict(to_test)
         print(result)
