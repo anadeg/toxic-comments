74711d15ad77bdcfc0b6e26627423c946ce5bb5e
АД <degterevastasa@gmail.com>
2023-05-22 13:12:47 +0300

    deleted useless model

--- toxicity_site/toxicity_model/models.py
+++ toxicity_site/toxicity_model/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-class SiteUser(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=50)
-
-
 class UserRequests(models.Model):
     username_id = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
     request_text = models.TextField(max_length=10000)
