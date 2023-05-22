30f6b266cd0513681f142ee2deed955210308671
АД <degterevastasa@gmail.com>
2023-05-22 13:14:02 +0300

    added urls

--- toxicity_site/toxicity_model/urls.py
+++ toxicity_site/toxicity_model/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 
 from . import views
 
+su = views.SiteUser()
+
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', su.home, name="home"),
     path('admin/', admin.site.urls, name="admin"),
     path('register/', views.RegisterUser.as_view(), name="register"),
     path('login/', views.LoginUser.as_view(), name="login"),
     path('logout/', views.logout_user, name="logout"),
-    path('user_page', views.user_page, name="user_page"),
-    path('requests_history', views.home, name="requests_history"),
-    path('make_requests', views.home, name="make_request"),
+    path('user_page', su.user_page, name="user_page"),
+    path('requests_history', su.get_requests, name="requests_history"),
+    path('make_requests', su.user_page, name="make_request"),
 ]
