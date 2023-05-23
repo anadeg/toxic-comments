a31f440645f4727df1aed9153b1835a5423dcc4b
АД <degterevastasa@gmail.com>
2023-05-23 12:57:43 +0300

    added filemode

--- toxicity_site/toxicity_model/logging_folder/logger_func.py
+++ toxicity_site/toxicity_model/logging_folder/logger_func.py
@@ -5,12 +5,8 @@ logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s [%(levelname)s] %(message)s",
                     handlers=[
                         logging.FileHandler("debug.log"),
-                        logging.StreamHandler(sys.stdout)]
-                    )
-modes = {"info": logging.INFO,
-         "error": logging.ERROR}
-
+                        logging.StreamHandler(sys.stdout)],
+                    filemode="w")
 
 def log_to_file_and_console(info):
-    # mylogs = logging.getLogger(__name__)
     logging.info(info)
