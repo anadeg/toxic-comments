c280da6566c3b65acf7d1bca78de3d7f133bc692
АД <degterevastasa@gmail.com>
2023-05-26 08:21:47 +0300

    disabled debug

--- toxicity_site/toxicity_site/settings.py
+++ toxicity_site/toxicity_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tx!2@oxwt66%2n-rhlmx7bo#kjbdse4ai&n@-42um9%-7t7^-k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -119,9 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+else:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
